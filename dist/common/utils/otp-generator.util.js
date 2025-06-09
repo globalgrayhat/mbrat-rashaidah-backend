7ca58dfcd316a7612d64beeebc5084a5c25c14e5
ძεՏκƦαʍ⸙ <deskram@mail.ru>
2025-06-10 01:06:08 +0300

    update and delete dist

--- dist/common/utils/otp-generator.util.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.generateOTP = generateOTP;
-function generateOTP(length) {
-    return Array.from({ length }, () => Math.floor(Math.random() * 10)).join('');
-}
-//# sourceMappingURL=otp-generator.util.js.map
--- dist/continents/dto/update-continent.dto.d.ts
+++
@@ -1,5 +0,0 @@
-import { CreateContinentDto } from './create-continent.dto';
-declare const UpdateContinentDto_base: import("@nestjs/mapped-types").MappedType<Partial<CreateContinentDto>>;
-export declare class UpdateContinentDto extends UpdateContinentDto_base {
-}
-export {};
